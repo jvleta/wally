b84e9af616b9d332d5ee96307975805382947352
Jimmy Leta <jimmy.leta@gmail.com>
2025-07-06 15:01:29 -0400

    committing initial attempt to create a form wizard

--- flaskapp/main.py
+++ flaskapp/main.py
@@ -1,41 +1,56 @@
 import os
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 from wally.fletcher import DIFF, DiffInput
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("FLASK_SECRET_KEY", "supersecret")
 
 
 @app.route("/", methods=["GET", "POST"])
-def plot():
-    # Default values
+def step1():
     default_input = {
         "num_gridpoints": 11,
         "alpha": 0.00001,
         "s": 0.5,
         "max_time": 3500.0,
-        "output_timesteps": "0.0,1500.0,3000.0",
     }
     if request.method == "POST":
-        num_gridpoints = int(request.form["num_gridpoints"])
-        alpha = float(request.form["alpha"])
-        s = float(request.form["s"])
-        max_time = float(request.form["max_time"])
-        output_timesteps = [
-            float(x) for x in request.form["output_timesteps"].split(",")
-        ]
-    else:
-        num_gridpoints = default_input["num_gridpoints"]
-        alpha = default_input["alpha"]
-        s = default_input["s"]
-        max_time = default_input["max_time"]
-        output_timesteps = [
-            float(x) for x in default_input["output_timesteps"].split(",")
-        ]
+        session["num_gridpoints"] = int(request.form["num_gridpoints"])
+        session["alpha"] = float(request.form["alpha"])
+        session["s"] = float(request.form["s"])
+        session["max_time"] = float(request.form["max_time"])
+        return redirect(url_for("step2"))
+    return render_template(
+        "step1.html", default_input=default_input, session=session
+    )
 
+
+@app.route("/step2", methods=["GET", "POST"])
+def step2():
+    default_timesteps = "0.0,1500.0,3000.0"
+    if request.method == "POST":
+        session["output_timesteps"] = request.form["output_timesteps"]
+        return redirect(url_for("results"))
+    return render_template(
+        "step2.html", default_timesteps=default_timesteps, session=session
+    )
+
+
+@app.route("/results")
+def results():
+    # Gather all data from session
+    num_gridpoints = session.get("num_gridpoints", 11)
+    alpha = session.get("alpha", 0.00001)
+    s = session.get("s", 0.5)
+    max_time = session.get("max_time", 3500.0)
+    output_timesteps = [
+        float(x)
+        for x in session.get("output_timesteps", "0.0,1500.0,3000.0").split(",")
+    ]
     diff_input = DiffInput(num_gridpoints, alpha, s, max_time, output_timesteps)
     results = DIFF(diff_input)
-    return render_template("plots.html", results=results, default_input=default_input)
+    return render_template("plots.html", results=results, default_input=session)
 
 
 if __name__ == "__main__":
